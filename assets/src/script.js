c818dd53d926c08ced593dc5af6495018eab6bcd
RomaArtur <arturromafurtado@gmail.com>
2025-11-13 12:25:06 -0300

    Ajuste de aspas e correção da cor cinza transparente

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,19 +1,19 @@
-const botao = document.getElementById('modo__noturno');
+const botao = document.getElementById("modo__noturno");
 const body = document.body;
-const icone = botao.querySelector('i');
+const icone = botao.querySelector("i");
 
-botao.addEventListener('click', () => {
-  body.classList.toggle('dark');
+botao.addEventListener("click", () => {
+  body.classList.toggle("dark");
 
-  if (body.classList.contains('dark')) {
-    icone.classList.remove('fa-moon');
-    icone.classList.add('fa-sun');
-    botao.textContent = 'Modo Claro'; 
-    botao.prepend(icone); 
+  if (body.classList.contains("dark")) {
+    icone.classList.remove("fa-moon");
+    icone.classList.add("fa-sun");
+    botao.textContent = "Modo Claro";
+    botao.prepend(icone);
   } else {
-    icone.classList.remove('fa-sun');
-    icone.classList.add('fa-moon');
-    botao.textContent = 'Modo Escuro'; 
-    botao.prepend(icone); 
+    icone.classList.remove("fa-sun");
+    icone.classList.add("fa-moon");
+    botao.textContent = "Modo Escuro";
+    botao.prepend(icone);
   }
 });
